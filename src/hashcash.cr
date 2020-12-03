64e3195d99e45993641b3f7363b8322a7aa3db1b
GabFitzgerald <gabfitzgeralddeveloper@gmail.com>
2020-12-04 09:02:32 +1000

    ref: optimised generate method

--- src/hashcash.cr
+++ src/hashcash.cr
@@ -8,7 +8,6 @@ module HashCash
   VERSION = "0.1.0"
 
   class Stamp
-    # put getters here
     getter resource
     getter bits
     getter date
@@ -17,24 +16,21 @@ module HashCash
 
     STAMP_VERSION = 1 # move??
 
-    # new_stamp = HashCash::Stamp.new("hello"")
-    #
-    # This creates a 20 bit hash cash stamp, which can be retrieved using
-    # the stamp_string() attribute reader method.
-    #
-    # Optionally, the parameters bits and date can be passed to the
-    # method to change the number of bits the stamp is worth and the issuance
-    # date (which is checked on the server for an expiry with a default
-    # deviance of 2 days, pass a Time object).
-
-    def initialize(@resource : String, @bits = 20, @date = Time.utc, @version = STAMP_VERSION)
-      @stamp_string = "" # generate_stamp_string
+    def initialize(
+      @resource : String,
+      @version = STAMP_VERSION,
+      @bits = 20,
+      @date = Time.utc,
+      @ext = ""
+    )
+      @stamp_string = ""
     end
 
-    def generate_stamp_string
+    # new_string_stamp = HashCash::Stamp.generate("resource@resource.com")
+    def generate
       random_string = Random::Secure.base64(12)
 
-      first_part = "#{@version}:#{@bits}:#{date_to_str(@date)}:#{@resource}::#{random_string}:"
+      first_part = "#{@version}:#{@bits}:#{date_to_str(@date)}:#{@resource}:#{@ext}:#{random_string}:"
 
       counter = 0
 
@@ -42,18 +38,8 @@ module HashCash
         test_stamp = first_part + Base64.encode(counter.to_s)
 
         # check that the first @bits bits are 0s
-        unpack = Digest::SHA1.digest(test_stamp) # change var name
-
-        last_byte = (@bits/8).floor.to_i
-
-        important_bytes = [] of UInt8
-        unpack[0..(last_byte - 1)].each do |byte|
-          important_bytes << byte
-        end
-        # puts bits % 8
-        important_bytes << unpack[last_byte].bits(0..@bits % 8)
-
-        @stamp_string = test_stamp if important_bytes.uniq == [0]
+        digest = Digest::SHA1.digest test_stamp
+        @stamp_string = test_stamp if check digest
 
         counter += 1
       end
@@ -68,6 +54,11 @@ module HashCash
       # resource =
 
       # parse date
+      # (@version, @bits, @date, @resource, ext, rand, counter) = stamp_string.split(':')
+      # puts @version
+      # puts rand
+      # puts counter
+
     end
 
     # verify the stamp
@@ -87,6 +78,17 @@ module HashCash
       else
         date.to_s("%y%m%d%H%M%S")
       end
+    end
+
+    private def check(digest : Bytes, bits = 20) : Bool
+      full_bytes = bits // 8
+      extra_bits = bits % 8
+
+      full = digest[0...full_bytes]
+      partial = digest[full_bytes]
+
+      return false unless full.all? 0
+      partial >> (8 - extra_bits) == 0
     end
   end
 end
