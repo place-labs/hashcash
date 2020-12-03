0f1ddb7f7abd344344b0ddbbd68c6954f11d79b2
GabFitzgerald <gabfitzgeralddeveloper@gmail.com>
2020-12-03 12:52:48 +1000

    style(hashcash.cr): crystal tool format & comment edits

--- src/hashcash.cr
+++ src/hashcash.cr
@@ -15,15 +15,12 @@ module HashCash
 
     STAMP_VERSION = 1 # needed???
 
-    # To construct a new HashCash::Stamp object, pass the :resource parameter
-    # to it, e.g.
-    #
-    # new_stamp = HashCash::Stamp.new(:resource => "hashcash@alech.de")
+    # new_stamp = HashCash::Stamp.new("hello"")
     #
     # This creates a 20 bit hash cash stamp, which can be retrieved using
     # the stamp_string() attribute reader method.
     #
-    # Optionally, the parameters :bits and :date can be passed to the
+    # Optionally, the parameters bits and date can be passed to the
     # method to change the number of bits the stamp is worth and the issuance
     # date (which is checked on the server for an expiry with a default
     # deviance of 2 days, pass a Time object).
@@ -36,23 +33,18 @@ module HashCash
       raise "date must be a Time object" unless date.class == Time
       @date = date # validate that this is a Time object
 
-      #initialised @stamp_string and @version
-
+      # initialised @stamp_string and @version
 
       # random_string = Base64.endcode(OpenSSL::Random.random_bytes(12))
       # puts random_string
 
     end
 
-    # Alternatively, a stamp can be passed to the constructor by passing
-    # it as a string to the :stamp parameter, e.g.
-    #
-    # pass_stamp = HashCash::Stamp.parse("1:20:060408:adam@cypherspace.org::1QTjaYd7niiQA/sc:ePa")
-
+    # pass_stamp = HashCash::Stamp.parse("1:20:060408:gab@place.technology::1QTjaYd7niiQA/sc:ePa")
 
     # verify the stamp
     def self.verify(resources : String, bits = 20)
-      # conditions that it would not be valid here 
+      # conditions that it would not be valid here
       # => false
 
       # otherwise
@@ -60,9 +52,3 @@ module HashCash
     end
   end
 end
-
-new_stamp = HashCash::Stamp.new("hashcash@alech.de")
-puts new_stamp
-
-verified_stamp = HashCash::Stamp.verify("hello")
-puts verified_stamp
