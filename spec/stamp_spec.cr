6846b097c2b5edd0953265f27bdee6bbae1abbfe
GabFitzgerald <gabfitzgeralddeveloper@gmail.com>
2021-01-06 16:47:25 +1000

    ref: valid and verigy TODO move to hashcash module level

---
+++ spec/stamp_spec.cr
@@ -0,0 +1,173 @@
+require "./spec_helper"
+
+describe Hashcash::Stamp do
+  it "should initialise a new hashcash instance" do
+    # with just resource arg
+    new_stamp = Hashcash::Stamp.new("gab@place.technology")
+
+    new_stamp.resource.should eq "gab@place.technology"
+    new_stamp.bits.should eq 20
+    new_stamp.date.hour.should eq Time.utc.hour
+    new_stamp.version.should eq 1
+    new_stamp.counter.should be_a Int32
+    new_stamp.rand.should be_a String
+
+    # with all of the args
+    custom_stamp = Hashcash::Stamp.new("hi@hello.com", 2, 16, Time.utc, "goodbye")
+
+    custom_stamp.resource.should eq "hi@hello.com"
+    custom_stamp.version.should eq 2
+    custom_stamp.bits.should eq 16
+    custom_stamp.date.hour.should eq Time.utc.hour
+  end
+
+  # test to_s class method
+  it "should convert a hashcash stamp instance to a string" do
+    new_stamp = Hashcash::Stamp.new("gab@place.technology").to_s
+    new_stamp.should be_a String
+    new_stamp.should start_with "1:20:"
+    new_stamp.should contain ":gab@place.technology::"
+
+    custom_stamp = Hashcash::Stamp.new("hi@hello.com", 2, 16, Time.utc(2016, 2, 15, 10, 20, 30), "goodbye").to_s
+    custom_stamp.should be_a String
+    custom_stamp.should start_with "2:16:160215102030:hi@hello.com:goodbye:"
+  end
+
+  # test update_counter class method
+  it "should generate a hashcash stamp string" do
+    new_stamp = Hashcash::Stamp.new("hello")
+    new_stamp.counter.should eq 0
+    # when counter is 0, string should end with 0 base64 encoded (MA==)
+    new_stamp.to_s.should end_with ":MA=="
+    # string should be invalid here
+    Hashcash::Stamp.parse(new_stamp.to_s).valid?("hello").should eq false
+
+    new_stamp.update_counter # this method might be renamed
+    new_stamp.counter.should be > 0
+
+    new_stamp_string = new_stamp.to_s
+    new_stamp_string.should contain ":hello::"
+    new_stamp_string.should start_with "1:20:"
+
+    # string should now be valid
+    Hashcash::Stamp.parse(new_stamp_string).valid?("hello").should eq true
+
+    # with all of the args
+    custom_stamp = Hashcash::Stamp.new("hi@hello.com", 1, 16, Time.utc(2016, 2, 15, 10, 20, 30), "goodbye")
+    custom_stamp.counter.should eq 0
+    custom_stamp.to_s.should end_with ":MA=="
+    Hashcash::Stamp.parse(new_stamp.to_s).valid?("hello", Time.utc(2016, 1, 15, 10, 20, 30)..Time.utc(2017, 2, 15, 10, 20, 30), 16).should eq false
+
+    custom_stamp.update_counter
+    custom_stamp.counter.should be > 0
+
+    custom_stamp_string = custom_stamp.to_s
+    custom_stamp_string.should start_with "1:16:160215102030:hi@hello.com:goodbye:"
+    Hashcash::Stamp.parse(custom_stamp_string).valid?("hi@hello.com", Time.utc(2016, 1, 15, 10, 20, 30)..Time.utc(2017, 2, 15, 10, 20, 30), 16).should eq true
+  end
+
+  # test parse class method
+  it "should parse a string to a Hashcash::Stamp" do
+    parsed_string = Hashcash::Stamp.parse("1:20:201206222555:resource::pOWgc88+uDuefr/o:MTMxNzg2MA==")
+
+    parsed_string.version.should eq 1
+    parsed_string.bits.should eq 20
+    parsed_string.date.should be_a Time
+    parsed_string.resource.should eq "resource"
+    parsed_string.ext.should eq ""
+    parsed_string.to_s.should eq "1:20:201206222555:resource::pOWgc88+uDuefr/o:MTMxNzg2MA=="
+    parsed_string.rand.should eq "pOWgc88+uDuefr/o"
+    parsed_string.counter.should eq 1317860
+  end
+
+  it "should not parse an invalid stamp" do
+    begin
+      Hashcash::Stamp.parse("invalid_stamp")
+    rescue e
+      e.should be_a(Exception)
+      e.message.should eq "invalid stamp format, should contain 6 colons (:)"
+    end
+
+    begin
+      Hashcash::Stamp.parse("2:12:160215102030:hello@email.com:bye:invalid_stamp:counter")
+    rescue e
+      e.message.to_s.should eq "stamp version 2 not supported"
+    end
+  end
+
+  # test is_for?
+  it "should check if the correct resource is in the stamp" do
+    parsed_string = Hashcash::Stamp.parse("1:20:201206222555:resource::pOWgc88+uDuefr/o:MTMxNzg2MA==")
+    parsed_string.is_for?("resource").should eq true
+    parsed_string.is_for?("not the resource").should eq false
+  end
+
+  # test expired?
+  it "should check if a stamp is expired" do
+    parsed_string = Hashcash::Stamp.parse("1:20:201207232233:hello::/AwX0LmTwb3g7nx9:NjAwNDcz")
+    parsed_string.expired?(Time.utc(2019, 12, 7, 23, 22, 33)..Time.utc(2021, 12, 7, 23, 22, 33)).should eq false
+    parsed_string.expired?(Time.utc(2019, 12, 7, 23, 22, 33)..Time.utc(2019, 12, 7, 23, 22, 33)).should eq true
+
+    parsed_string2 = Hashcash::Stamp.parse("1:20:201107232233:hello::/AwX0LmTwb3g7nx9:NjAwNDcz")
+    parsed_string2.expired?.should eq true
+
+    time = Time.utc.to_s("%y%m%d%H%M%S")
+    parsed_string3 = Hashcash::Stamp.parse("1:20:#{time}:hello::/AwX0LmTwb3g7nx9:NjAwNDcz")
+    parsed_string3.expired?.should eq false
+  end
+
+  # test correct_bits?
+  it "should check if a stamp has the correct number of 0 bits" do
+    parsed_string = Hashcash::Stamp.parse("1:20:210106051438:hello::0Ni8oRBm7K0noD1j:NDEyNTQ5")
+    parsed_string.correct_bits?(20).should eq true
+    parsed_string.correct_bits?(22).should eq false
+    parsed_string.correct_bits?.should eq true
+
+    invalid_stamp = Hashcash::Stamp.parse("1:19:201207232233:hello::/AwX0LmTwb3g7nx9:NjAwNDcz")
+    invalid_stamp.correct_bits?(20).should eq false
+
+    # custom bits
+    parsed_stamp2 = Hashcash::Stamp.parse("1:12:210106054523:hello::gyJzsWmtmKpDiWRP:Nzc1Mw==")
+    parsed_stamp2.correct_bits?(12).should eq true
+    parsed_stamp2.correct_bits?.should eq true
+    parsed_stamp2.correct_bits?(20).should eq false
+    parsed_stamp2.correct_bits?(30).should eq false
+  end
+
+  # test valid? class method
+  it "should check if a stamp is valid - not expired, for the right resource and has enough 0 bits" do
+    hashcash = Hashcash::Stamp.parse("1:20:210106063543:hello::/MD1O8MscgavDI6z:MzkyMjM3Ng==")
+
+    validity = hashcash.valid?("hello", Time.utc(2019, 2, 15, 10, 20, 30)..Time.utc(2050, 2, 15, 10, 20, 30))
+    validity.should eq true
+
+    invalid_stamp = hashcash.valid?("goodbye")
+    invalid_stamp.should eq false
+
+    invalid_stamp2 = hashcash.valid?("hello", Time.utc(2016, 2, 15, 10, 20, 30)..Time.utc(2017, 2, 15, 10, 20, 30))
+    invalid_stamp2.should eq false
+
+    invalid_stamp3 = hashcash.valid?("hello", Time.utc(2019, 2, 15, 10, 20, 30)..Time.utc(2050, 2, 15, 10, 20, 30), 40)
+    invalid_stamp3.should eq false
+  end
+
+  # test verify method
+  it "should raise error for invalid stamps, otherwise return nil" do
+    hashcash = Hashcash::Stamp.parse("1:20:210106063543:hello::/MD1O8MscgavDI6z:MzkyMjM3Ng==")
+
+    validity = hashcash.valid!("hello", Time.utc(2019, 2, 15, 10, 20, 30)..Time.utc(2050, 2, 15, 10, 20, 30))
+    validity.should eq nil
+
+    begin
+      invalid = hashcash.valid!("hello", Time.utc(2018, 2, 15, 10, 20, 30)..Time.utc(2019, 2, 15, 10, 20, 30))
+    rescue e
+      e.message.should eq "Stamp is expired/not yet valid"
+    end
+
+    begin
+      invalid = hashcash.valid!("goodbye")
+    rescue e
+      e.message.should eq "Stamp is not valid for the given resource(s)."
+    end
+  end
+end
---
+++ src/stamp.cr
@@ -0,0 +1,120 @@
+class Hashcash::Stamp
+  STAMP_VERSION = 1
+  getter version, bits, date, resource, ext, rand
+  property counter
+
+  def initialize(
+    @resource : String,
+    @version = STAMP_VERSION,
+    @bits = 20,
+    @date = Time.utc,
+    @ext = "",
+    @rand = Random::Secure.base64(12),
+    @counter = 0
+  )
+  end
+
+  def update_counter
+    until check (Digest::SHA1.digest self.to_s), bits
+      @counter += 1
+    end
+  end
+
+  def to_s(io : IO) : Nil
+    io << version
+    io << ':'
+    io << bits
+    io << ':'
+    io << date.to_s("%y%m%d%H%M%S")
+    io << ':'
+    io << resource
+    io << ':'
+    io << ext
+    io << ':'
+    io << rand
+    io << ':'
+    io << Base64.encode(counter.to_s).chomp
+  end
+
+  def self.parse(stamp : String)
+    # raise "invalid stamp format, should contain 6 colons (:)" unless correct_format?(stamp)
+    raise "invalid stamp format, should contain 6 colons (:)" unless stamp.count(':') == 6
+    parts = stamp.split(":")
+    version, bits, date, resource, ext, rand, counter = parts
+
+    raise "stamp version #{version} not supported" unless version == STAMP_VERSION.to_s
+
+    Hashcash::Stamp.new(
+      resource,
+      version.to_i,
+      bits.to_i,
+      Time.parse_utc(date, "%y%m%d%H%M%S"),
+      ext,
+      rand,
+      Base64.decode_string(counter).to_i
+    )
+  end
+
+  def is_for?(resource : String) : Bool
+    self.resource == resource
+  end
+
+  def expired?(window : Range(Time, Time) = 2.days.ago..2.days.from_now) : Bool
+    !window.includes?(date)
+  end
+
+  def correct_bits?(bits = bits) : Bool
+    digest = Digest::SHA1.digest self.to_s
+    check(digest, bits)
+  end
+
+  def valid?(
+    resource : String,
+    time_window = 2.day.ago..2.days.from_now,
+    bits = 20
+  ) : Bool
+    case
+    when !(self.is_for?(resource))
+      false
+    when self.expired?(time_window)
+      false
+    when !self.correct_bits?(bits)
+      false
+    else
+      true
+    end
+  end
+
+  def valid!(
+    resource : String,
+    time_window = 2.day.ago..2.days.from_now,
+    bits = 20
+  ) : Nil
+    case
+    when !self.is_for?(resource)
+      raise "Stamp is not valid for the given resource(s)."
+    when self.expired?(time_window)
+      raise "Stamp is expired/not yet valid"
+    when !self.correct_bits?(bits)
+      raise "Invalid stamp, not enough 0 bits"
+    else
+      true
+    end
+  end
+
+  # TODO update method to count the number of 0s at the start rather than check it matches
+  private def check(digest : Bytes, bits = 20) : Bool
+    full_bytes = bits // 8
+    extra_bits = bits % 8
+
+    full = digest[0...full_bytes]
+    partial = digest[full_bytes]
+
+    return false unless full.all? 0
+    partial >> (8 - extra_bits) == 0
+  end
+
+  private def correct_format?(stamp : String) : Bool
+    stamp.count(':') == 6
+  end
+end
